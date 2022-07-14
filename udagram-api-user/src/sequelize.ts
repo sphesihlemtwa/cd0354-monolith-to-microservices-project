afd04569c8c218e9ea5f2caaaf450cd669274bd3
sphesihlemtwa <73141064+sphesihlemtwa@users.noreply.github.com>
2022-07-14 14:46:41 +0200

    Chnaged backend into user and feed service

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,14 @@
+import {Dialect} from 'sequelize';
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect as Dialect,
+  'storage': ':memory:',
+});
