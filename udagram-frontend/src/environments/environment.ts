d8d7d35eea76033a65e71ea8570616b5cf29aab3
sphesihlemtwa <73141064+sphesihlemtwa@users.noreply.github.com>
2022-07-15 22:59:47 +0200

    Made changes to production environment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a77f57cd98495404791f038f8913b071-1588487607.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
